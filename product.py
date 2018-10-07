385c00cb209de42272b27e2600b9a5f37753a11c
Bryan <xbbryan@yahoo.com.tw>
2018-10-07 17:16:28 +0800

    加入欄位名稱

--- product.py
+++ product.py
@@ -4,6 +4,12 @@ while True:
     if name == 'q':
         break
     price = input('請輸入商品價格: ')
+    price = int(price)
     p = [name,price]
     products.append(p)
 print(products)
+
+with open('product.csv','w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
